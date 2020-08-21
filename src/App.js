5eba05751ff899af6243df8518269d3190475c33
Freddy Ramirez <fj.ram.contr@gmail.com>
2020-08-21 00:01:52 -0600

    UI funcional

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Controlled as CodeMirror} from 'react-codemirror2';
+import { Container, Row, Col, Button } from 'reactstrap';
+
 import './App.css';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
+import 'codemirror/theme/tomorrow-night-eighties.css'
+import 'codemirror/mode/javascript/javascript.js'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [code, setCode] = useState("");
+    const [cursor, setCursor] = useState({
+        ch: 0,
+        line: 0
+    })
+    const [consola, setConsola] = useState("> valor de x: 2");
+
+    return (
+        <Container fluid className="App">
+            <Row>
+                <Col sm="8" className="editor">
+                    <CodeMirror 
+                        value={code}
+                        options={{
+                            theme: 'material',
+                            mode: 'javascript',
+                            lineNumbers: true,
+                            indentUnit: 4
+                        }}
+                        onBeforeChange={(editor, data, value) => {
+                            setCode(value)
+                        }}
+                        onChange={(editor, data, value) => {
+                        }}
+                        onCursorActivity={(editor) => {
+                            setCursor(editor.getCursor())
+                        }}
+                    />
+                    <div className="ColRow">
+                        col: {cursor.ch} line: {cursor.line + 1}
+                    </div>
+                </Col>
+                <Col>
+                    <h2>Reportes</h2>
+
+                    <Row>
+                        <Col>
+                            <Button block>Traducir</Button>
+                        </Col>
+                        <Col>
+                            <Button block>Ejecutar</Button>
+                        </Col>
+                    </Row>
+                    
+                    <div className="reportes">
+                        
+                    </div>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    Consola
+                    <div className="consola">
+                        <CodeMirror 
+                            value={consola}
+                            options={{
+                                readOnly: true,
+                                theme: 'material'
+                            }}
+                        />
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default App;
